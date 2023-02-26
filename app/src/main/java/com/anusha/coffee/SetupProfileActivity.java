7cbcb28e41811f5e34066d4a6fa2bd69fda14079
curiouscurrent <divakardivi1963@gmail.com>
2023-02-26 08:02:08 +0530

    Initial commit

---
+++ app/src/main/java/com/anusha/coffee/SetupProfileActivity.java
@@ -0,0 +1,139 @@
+package com.anusha.coffee;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.ProgressDialog;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+import com.anusha.coffee.databinding.ActivitySetupProfileBinding;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.appcheck.FirebaseAppCheck;
+import com.google.firebase.appcheck.debug.DebugAppCheckProviderFactory;
+import com.google.firebase.appcheck.playintegrity.PlayIntegrityAppCheckProviderFactory;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+
+import android.net.Uri;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+public class SetupProfileActivity extends AppCompatActivity {
+
+    ActivitySetupProfileBinding binding;
+    FirebaseAuth auth;
+    FirebaseDatabase database;
+    FirebaseStorage storage;
+
+    ActivityResultLauncher<String> mgetContent;
+    ImageView imageView;
+    Button PickImage;
+    Uri selectedImage;
+    ProgressDialog dialog;
+
+    private void initFirebase() {
+        FirebaseApp.initializeApp(this);
+        FirebaseAppCheck firebaseAppCheck = FirebaseAppCheck.getInstance();
+        firebaseAppCheck.installAppCheckProviderFactory(DebugAppCheckProviderFactory.getInstance());
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivitySetupProfileBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+
+
+        database = FirebaseDatabase.getInstance();
+        storage = FirebaseStorage.getInstance();
+        auth = FirebaseAuth.getInstance();
+
+        binding.imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mgetContent.launch("image/*");
+            }
+        });
+
+
+        binding.continueBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = binding.nameBox.getText().toString();
+                if(name.isEmpty()) {
+                    binding.nameBox.setError("Please type a name");
+                    return;
+                }
+
+
+                if(selectedImage != null)
+                {
+                    StorageReference reference = storage.getReference().child("Profiles").child(auth.getUid());
+                    reference.putFile(selectedImage).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                            if(task.isSuccessful())
+                            {
+                                reference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                    @Override
+                                    public void onSuccess(Uri uri) {
+                                        String imageUrl = uri.toString();
+                                        String uid = auth.getUid();
+                                        String phone = auth.getCurrentUser().getPhoneNumber();
+                                        String name = binding.nameBox.getText().toString();
+
+                                        User user = new User(uid,name,phone,imageUrl);
+
+                                        database.getReference()
+                                                .child("users")
+                                                .child(uid)
+                                                .setValue(user)
+                                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                    @Override
+                                                    public void onSuccess(Void unused) {
+                                                        Intent intent = new Intent(SetupProfileActivity.this, UserActivity.class);
+                                                        startActivity(intent);
+                                                        finish();
+                                                    }
+                                                });
+                                    }
+                                });
+                            }
+                        }
+                    });
+                }
+
+            }
+        });
+
+
+        mgetContent = registerForActivityResult(new ActivityResultContracts.GetContent(), new ActivityResultCallback<Uri>() {
+            @Override
+            public void onActivityResult(Uri result) {
+                binding.imageView.setImageURI(result);
+                selectedImage = result;
+            }
+        });
+
+
+
+
+    }}
