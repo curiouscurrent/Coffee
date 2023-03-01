35bd79cdeaf9946433e78e393da90c4b612db39e
curiouscurrent <divakardivi1963@gmail.com>
2023-03-01 14:49:02 +0530

    seventh commit-AR features

--- app/src/main/java/com/anusha/coffee/Activities/MainActivity.java
+++ app/src/main/java/com/anusha/coffee/Activities/MainActivity.java
@@ -8,13 +8,17 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
-import com.anusha.coffee.CustomArFragment;
+import com.anusha.coffee.Adapters.CustomArFragment;
 import com.anusha.coffee.R;
 import com.anusha.coffee.Models.User;
 import com.anusha.coffee.Adapters.UsersAdapter;
 import com.anusha.coffee.databinding.ActivityMainBinding;
+import com.google.ar.core.AugmentedFace;
+import com.google.ar.core.Frame;
 import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.rendering.Renderable;
 import com.google.ar.sceneform.rendering.Texture;
+import com.google.ar.sceneform.ux.AugmentedFaceNode;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
@@ -22,6 +26,7 @@ import com.google.firebase.database.ValueEventListener;
 
 
 import java.util.ArrayList;
+import java.util.Collection;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,18 +39,50 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<User> users;
     UsersAdapter usersAdapter;
 
-
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        CustomArFragment customArFragment = (CustomArFragment) getSupportFragmentManager().findFragmentById(R.id.arFragment);
+        ModelRenderable.builder()
+                .setSource(this,R.raw.fox_face)
+                .build()
+                .thenAccept(renderable -> {
+                    modelRenderable = renderable;
+                    modelRenderable.setShadowCaster(false);
+                    modelRenderable.setShadowReceiver(false);
 
+                        });
 
+        Texture.builder()
+                .setSource(this,R.drawable.fox_face_mesh_texture)
+                .build()
+                .thenAccept(texture -> this.texture = texture);
+
+        customArFragment.getArSceneView().setCameraStreamRenderPriority(Renderable.RENDER_PRIORITY_FIRST);
+        customArFragment.getArSceneView().getScene().addOnUpdateListener(frameTime -> {
+
+            if(modelRenderable == null || texture == null)
+                return;;
+
+            Frame frame = customArFragment.getArSceneView().getArFrame();
+
+            Collection<AugmentedFace> augmentedFaces = frame.getUpdatedTrackables(AugmentedFace.class);
+
+            for(AugmentedFace augmentedFace : augmentedFaces) {
+                if(isAdded)
+                    return;
+
+                AugmentedFaceNode augmentedFaceNode = new AugmentedFaceNode(augmentedFace);
+                augmentedFaceNode.setParent(customArFragment.getArSceneView().getScene());
+                augmentedFaceNode.setFaceRegionsRenderable(modelRenderable);
+                augmentedFaceNode.setFaceMeshTexture(texture);
+
+                isAdded = true;
+            }
+        });
 
         database = FirebaseDatabase.getInstance();
         users = new ArrayList<>();
